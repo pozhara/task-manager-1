efece10e48254a7b88efa4993f31dcf0c05249ef
pozhara <poliazhara04@gmail.com>
2023-03-24 17:41:33 +0000

    Initial commit

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format:"dd mmmm, yyyy",
+      i18n: {done:"Select"}
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
+  });
